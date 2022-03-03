a17ebd425a180f1d26a8a4babfe0fb46e7203fa1
ICE0208 <doky3307@naver.com>
2022-03-03 12:12:38 +0900

    Upload Mistake

--- Momonton/js/logout.js
+++
@@ -1,8 +0,0 @@
-const logoutButton = document.querySelector(".logout");
-
-logoutButton.addEventListener("click", function(event) {
-    localStorage.removeItem(USERNAME_KEY);
-    logoutForm.classList.add(HIDDEN_CLASSNAME);
-
-    location.reload();
-})
--- Momonton/js/search.js
+++
@@ -1,8 +0,0 @@
-const searchForm = document.querySelector('#search-form');
-
-searchForm.addEventListener('submit', event => {
-    event.preventDefault();
-    window.open('about:blank').location.href = `https://www.google.com/search?q=${searchInput.value}`;
-    searchForm.reset();
-    searchInput.blur();
-});
